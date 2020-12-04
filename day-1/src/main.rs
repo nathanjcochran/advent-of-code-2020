fba98e213485bdae66b33e8a0187795598257ba7
Nathaniel J Cochran <nathanjcochran@gmail.com>
2020-12-04 18:56:33 -0500

    Day 1, Problem 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -7,13 +7,36 @@ fn main() {
         .map(|s| s.parse().expect("Invalid integer"))
         .collect();
 
-    for i in &numbers {
-        for j in &numbers {
+    problem1(&numbers);
+    problem2(&numbers);
+}
+
+fn problem1(numbers: &Vec<i32>) {
+    println!("Problem 1");
+    for i in numbers {
+        for j in numbers {
             if i + j == 2020 {
                 println!("i: {}", i);
                 println!("j: {}", j);
                 println!("answer: {}", i * j);
                 return;
+            }
+        }
+    }
+}
+
+fn problem2(numbers: &Vec<i32>) {
+    println!("Problem 2");
+    for i in numbers {
+        for j in numbers {
+            for k in numbers {
+                if i + j + k == 2020 {
+                    println!("i: {}", i);
+                    println!("j: {}", j);
+                    println!("k: {}", k);
+                    println!("answer: {}", i * j * k);
+                    return;
+                }
             }
         }
     }
