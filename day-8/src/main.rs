cfe1876a615f5545f54362b05eef6f7e05a62377
Nathaniel J Cochran <nathanjcochran@gmail.com>
2020-12-08 21:15:49 -0500

    Day 8, Problem 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,6 +1,7 @@
 //use std::collections::HashMap;
 use std::fs;
 
+#[derive(Clone)]
 struct Instruction {
     op: String,
     arg: i32,
@@ -26,6 +27,7 @@ fn main() {
         .collect();
 
     problem1(&instructions);
+    problem2(&instructions);
 }
 
 fn problem1(instructions: &Vec<Instruction>) {
@@ -55,4 +57,59 @@ fn problem1(instructions: &Vec<Instruction>) {
     }
 
     println!("Accumulator: {}", accumulator);
+}
+
+fn problem2(instructions: &Vec<Instruction>) {
+    println!("Problem 2");
+
+    for (i, instruction) in instructions.iter().enumerate() {
+        let replacement = match instruction.op.as_str() {
+            "acc" => continue,
+            "jmp" => Instruction {
+                op: "nop".to_string(),
+                arg: instruction.arg,
+            },
+            "nop" => Instruction {
+                op: "jmp".to_string(),
+                arg: instruction.arg,
+            },
+            _ => continue,
+        };
+
+        let mut replaced = instructions.to_vec();
+        replaced[i] = replacement;
+        let (accumulator, finished) = helper(&replaced);
+        if finished {
+            println!("Accumulator: {}", accumulator);
+            return;
+        }
+    }
+}
+
+fn helper(instructions: &Vec<Instruction>) -> (i32, bool) {
+    let mut position = 0;
+    let mut accumulator = 0;
+    let mut visited = vec![false; instructions.len()];
+    loop {
+        if position == instructions.len() {
+            return (accumulator, true);
+        }
+        if visited[position] == true {
+            return (0, false);
+        }
+        visited[position] = true;
+
+        let instruction = &instructions[position];
+        match instruction.op.as_str() {
+            "acc" => {
+                accumulator += instruction.arg;
+                position += 1;
+            }
+            "jmp" => position = ((position as i32) + instruction.arg) as usize,
+            "nop" => position += 1,
+            _ => {
+                panic!("Invalid operation: {}", instruction.op);
+            }
+        }
+    }
 }
